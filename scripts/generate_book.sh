ba489bc8170bc75ae17a7bedbf567ae64a02257d
Pierre Besson <pierre.besson@parity.io>
2022-11-15 21:25:18 +0100

    separate mainnets from testnets in sidebar

--- scripts/generate_book.sh
+++ scripts/generate_book.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tera --template templates/home.md.tera directory/src/chains.json > directory/src/HOME.md
-tera --template templates/summary.md.tera directory/src/chains.json > directory/src/SUMMARY.md
+tera  --template templates/home.md.tera directory/src/chains.json --include > directory/src/HOME.md
+tera --template templates/summary.md.tera directory/src/chains.json --include > directory/src/SUMMARY.md
 mdbook build directory/
