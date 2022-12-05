827c01a13c47a574022457a3d786db81e52140f4
Chevdor <chevdor@users.noreply.github.com>
2022-12-05 14:45:29 +0100

    Add registry (#11)
    * Add registry generation
    
    * Add information to the home of the mdbook

---
+++ scripts/generate_registry.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PATH_INFO="chain_info"
+PATH_DATA="../chain_data"
+PATH_MD="../directory/src/chains"
+TEMPLATE="templates/registry.json.tera"
+
+mkdir -p "$PATH_DATA"
+mkdir -p "$PATH_MD"
+
+pwd
+tera --template "$TEMPLATE" directory/src/chains.json | jq > directory/src/registry.json
