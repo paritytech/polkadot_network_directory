a19c6e577babd080db27a827303ff31edf84a9ec
Chevdor <chevdor@gmail.com>
2023-06-12 14:26:53 +0200

    Add schema check

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -5,6 +5,9 @@ if ! which yamlcheck >/dev/null; then
 fi
 echo "Using yamlcheck $(yamlcheck --version)"
 
+overall_tmp=$(mktemp)
+
+echo 0 > $overall_tmp
 find chain_info -name "*.yaml" | while read -r f; do
     yamlcheck check -s ./schemas/pnd_chain-schema.json --file "$f" > /dev/null
     res=$?
@@ -12,8 +15,12 @@ find chain_info -name "*.yaml" | while read -r f; do
         status="OK "
     else
         status="ERR"
+        echo 1 > $overall_tmp
     fi
     short_name=$(basename "$f" | cut -d "." -f1)
 
     echo "$status : $short_name"
 done
+
+overall=$(cat $overall_tmp)
+exit $overall
