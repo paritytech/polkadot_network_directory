6eff95097dc033094840d4d959c10214fbc5be15
Chevdor <chevdor@users.noreply.github.com>
2023-06-12 16:25:52 +0200

    Add schema check (#64)
    * Add schema check
    
    * Fix script
    
    * Switch to Container image for yamlcheck
    
    * Add podman support
    
    * Fix actual schema issues

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-if ! which yamlcheck >/dev/null; then
-    cargo install yamlcheck
-fi
-echo "Using yamlcheck $(yamlcheck --version)"
+ENGINE=${ENGINE:-podman}
 
-find chain_info -name "*.yaml" | while read -r f; do
-    yamlcheck check -s ./schemas/pnd_chain-schema.json --file "$f" > /dev/null
+function yc {
+    $ENGINE run --rm -t -v $PWD:/workdir docker.io/chevdor/yamlcheck $@
+}
+
+echo "Using yamlcheck $(yc --version)"
+
+overall_tmp=$(mktemp)
+
+echo 0 > $overall_tmp
+find chain_info -name "*.yaml" | sort | while read -r f; do
+    yc check -s ./schemas/pnd_chain-schema.json --file "$f" > /dev/null
     res=$?
-    if (( res == 0 )) then
+    if (( res == 0 )); then
         status="OK "
     else
         status="ERR"
+        echo 1 > $overall_tmp
     fi
     short_name=$(basename "$f" | cut -d "." -f1)
 
     echo "$status : $short_name"
 done
+
+overall=$(cat $overall_tmp)
+exit $overall
