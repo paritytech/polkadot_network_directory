1913bc3d2cd6b0011c8fcd06ba6fce8806493f45
Chevdor <chevdor@gmail.com>
2023-06-12 14:34:32 +0200

    Fix script

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -11,7 +11,7 @@ echo 0 > $overall_tmp
 find chain_info -name "*.yaml" | while read -r f; do
     yamlcheck check -s ./schemas/pnd_chain-schema.json --file "$f" > /dev/null
     res=$?
-    if (( res == 0 )) then
+    if (( res == 0 )); then
         status="OK "
     else
         status="ERR"
